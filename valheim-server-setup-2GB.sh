70a418ba321b24fb0720a3fdc7ac980899cff954
penM000 <33054826+penM000@users.noreply.github.com>
2021-02-23 20:50:10 +0900

    Add files via upload

--- valheim-server-setup-2GB.sh
+++ valheim-server-setup-2GB.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+dpkg --add-architecture i386
 apt update
 apt upgrade -y
 apt install -y unzip binutils jq netcat lib32gcc1 lib32stdc++6 steamcmd libsdl2-2.0-0:i386
+apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 steamcmd
+
+
 
 fallocate -l 2G /swapfile2GB
 chmod 600 /swapfile2GB
